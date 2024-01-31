af8f406bfc138eb9c4d22b511ee231601ecec63f
ezllabsChoiYeongho <ezllabsChoiYeongho@users.noreply.github.com>
2024-01-31 00:29:35 +0000

    hello new commit!2

--- src/styles/design-tokens/color/sys/lightTheme.ts
+++ src/styles/design-tokens/color/sys/lightTheme.ts
@@ -1,4 +1,4 @@
-export const primary = "#410e0b" as const;
+export const primary = "#601410" as const;
 export const onPrimary = "#cac4d0" as const;
 export const primaryContainer = "#9a82db" as const;
 export const onPrimaryContainer = "#21005d" as const;
