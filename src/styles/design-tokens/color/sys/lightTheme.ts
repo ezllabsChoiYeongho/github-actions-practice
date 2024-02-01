db5f5a7a2fd0f4837b20be678ea061de008be8f5
최영호 <156644173+ezllabsChoiYeongho@users.noreply.github.com>
2024-02-01 16:25:15 +0900

    테스트 PR (#57)
    * chore: change color
    
    * chore: 토큰 빌드 결과물 업데이트
    
    * chore: change color
    
    * chore: 토큰 빌드 결과물 업데이트
    
    * chore: add branches
    
    ---------
    
    Co-authored-by: ezllabsChoiYeongho <ezllabsChoiYeongho@users.noreply.github.com>

--- src/styles/design-tokens/color/sys/lightTheme.ts
+++ src/styles/design-tokens/color/sys/lightTheme.ts
@@ -1,4 +1,4 @@
-export const primary = "#dc362e" as const;
+export const primary = "#b3261e" as const;
 export const onPrimary = "#6750a4" as const;
 export const primaryContainer = "#410e0b" as const;
 export const onPrimaryContainer = "#21005d" as const;
