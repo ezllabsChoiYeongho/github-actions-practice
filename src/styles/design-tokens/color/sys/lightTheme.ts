e995aa7724f975e1576c5c7545cd360997a4a413
ezllabsChoiYeongho <ezllabsChoiYeongho@users.noreply.github.com>
2024-02-01 16:49:44 +0000

    chore: 토큰 빌드 결과물 업데이트

--- src/styles/design-tokens/color/sys/lightTheme.ts
+++ src/styles/design-tokens/color/sys/lightTheme.ts
@@ -1,4 +1,4 @@
-export const primary = "#601410" as const;
+export const primary = "#36343b" as const;
 export const onPrimary = "#ffffff" as const;
 export const primaryContainer = "#eaddff" as const;
 export const onPrimaryContainer = "#21005d" as const;
